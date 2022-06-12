9f4c7568b1fcb54b845023f7e3d73265acbcef68
PonomareVlad <Git@PonomareVlad.ru>
2022-06-12 22:38:02 +0500

    Build-step test 8

--- api/index.mjs
+++ api/index.mjs
@@ -1,38 +1,15 @@
+import {template, options} from "../src/template.mjs"
 import {errorHandler} from "#lib/errorHandler.mjs"
 import RenderStream from "@svalit/vercel"
 import RenderPage from "../src/index.mjs"
-import {readFileSync} from "fs"
-import {html} from "lit"
-
-import '../components/app-page.mjs'
 
 const RenderPageStream = RenderPage(RenderStream)
-
-const dev = process.env.VERCEL_ENV === 'development', options = {
-    dev,
-    shim: true,
-    importMapOptions: {
-        cache: dev,
-        ignore: ['mongodb'],
-        inputMap: {
-            imports: {
-                "#root/": "/",
-                "#lib/": "/lib/",
-                "#utils": "/lib/utils.mjs",
-                "#db": "/lib/db/client.mjs",
-            }
-        }
-    },
-    imports: ['/components/app-page.mjs'],
-    content: {head: readFileSync(new URL('../includes/head.html', import.meta.url))}
-}
 
 export default (req, res) => {
     try {
         const url = `${req.headers['x-forwarded-proto'].split(',').shift()}://${req.headers['x-forwarded-host']}${req.url}`
         const page = new RenderPageStream({...options, req, res, meta: {title: 'LCMS', url}})
-        return page.renderTemplate(({meta: {url, setMeta}}) => html`
-            <app-page url="${url}" .setMeta="${setMeta}"></app-page>`)
+        return page.renderTemplate(template)
     } catch (e) {
         return errorHandler(e, res)
     }
