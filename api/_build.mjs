4fd43c267f6485fa9f726cd1b8cb598731b714c1
PonomareVlad <Git@PonomareVlad.ru>
2022-06-12 22:19:01 +0500

    Build-step test 7

--- api/_build.mjs
+++ api/_build.mjs
@@ -1,4 +1,4 @@
-import {readFileSync, mkdirSync} from "fs"
+import {readFileSync, mkdirSync, writeFileSync} from "fs"
 import render from "svalit/prerender.mjs"
 import RenderPage from "../src/index.mjs"
 import RenderThread from "svalit"
@@ -8,29 +8,31 @@ import '../components/app-page.mjs'
 
 const RenderPageThread = RenderPage(RenderThread)
 
-const dev = process.env.VERCEL_ENV === 'development', publicDir = new URL('../public/', import.meta.url), options = {
-    dev,
-    shim: true,
-    importMapOptions: {
-        cache: dev,
-        ignore: ['mongodb'],
-        inputMap: {
-            imports: {
-                "#root/": "/",
-                "#lib/": "/lib/",
-                "#utils": "/lib/utils.mjs",
-                "#db": "/lib/db/client.mjs",
+const dev = process.env.VERCEL_ENV === 'development', publicDir = new URL('../.vercel/output/static/', import.meta.url),
+    options = {
+        dev,
+        shim: true,
+        importMapOptions: {
+            cache: dev,
+            ignore: ['mongodb'],
+            inputMap: {
+                imports: {
+                    "#root/": "/",
+                    "#lib/": "/lib/",
+                    "#utils": "/lib/utils.mjs",
+                    "#db": "/lib/db/client.mjs",
+                }
             }
-        }
-    },
-    imports: ['/components/app-page.mjs'],
-    content: {head: readFileSync(new URL('../includes/head.html', import.meta.url))}
-}, template = ({meta: {url, setMeta}}) => html`
-    <app-page url="${url}" .setMeta="${setMeta}"></app-page>`
+        },
+        imports: ['/components/app-page.mjs'],
+        content: {head: readFileSync(new URL('../includes/head.html', import.meta.url))}
+    }, template = ({meta: {url, setMeta}}) => html`
+            <app-page url="${url}" .setMeta="${setMeta}"></app-page>`
 
 console.debug('Building to path ', publicDir.href)
 
 mkdirSync(publicDir, {recursive: true})
+writeFileSync(new URL('../config.json', publicDir), JSON.stringify({version: 3}))
 
 const result = await render({
     renderOptions: {...options, meta: {title: 'LCMS'}},
