6f197970a73deabe7bb55b1095633c4e075ec4e3
PonomareVlad <Git@PonomareVlad.ru>
2022-06-12 21:36:09 +0500

    Build-step test

---
+++ api/_build.mjs
@@ -0,0 +1,38 @@
+import render from "svalit/prerender.mjs"
+import RenderPage from "../src/index.mjs"
+import RenderThread from "svalit"
+import {readFileSync} from "fs"
+import {html} from "lit"
+
+import '../components/app-page.mjs'
+
+const RenderPageThread = RenderPage(RenderThread)
+
+const dev = process.env.VERCEL_ENV === 'development', options = {
+    dev,
+    shim: true,
+    importMapOptions: {
+        cache: dev,
+        ignore: ['mongodb'],
+        inputMap: {
+            imports: {
+                "#root/": "/",
+                "#lib/": "/lib/",
+                "#utils": "/lib/utils.mjs",
+                "#db": "/lib/db/client.mjs",
+            }
+        }
+    },
+    imports: ['/components/app-page.mjs'],
+    content: {head: readFileSync(new URL('../includes/head.html', import.meta.url))}
+}, template = ({meta: {url, setMeta}}) => html`
+    <app-page url="${url}" .setMeta="${setMeta}"></app-page>`
+
+const result = await render({
+    renderOptions: {...options, meta: {title: 'LCMS'}},
+    publicDir: new URL('../', import.meta.url),
+    renderClass: RenderPageThread,
+    template
+})
+
+console.debug('Complete build', result.length, 'routes')
